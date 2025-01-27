6c339f966fba1742a29d3dacf4a59b8f8b9bef3d
Alex test <choppydreams7@gmail.com>
2025-01-27 14:47:55 +0500

    Реализовано задание №2

--- OOP_homework/OOP_homework.py
+++ OOP_homework/OOP_homework.py
@@ -17,6 +17,25 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'''Имя: {self.name}
+Фамилия: {self.surname}
+Средняя оценка за домашние задания: {self.calculate_avg_grade()}
+Курсы в процессе изучения: {", ".join(self.courses_in_progress)}
+Завершенные курсы: {", ".join(self.finished_courses)}'''
+
+    def calculate_avg_grade(self):
+        if not self.grades:
+            return 0
+        all_grades = [grade for grades in self.grades.values() for grade in grades]
+        return sum(all_grades) / len(all_grades)
+
+    def __lt__(self, other):
+        return self.calculate_avg_grade() < other.calculate_avg_grade()
+
+    def __eq__(self, other):
+        return self.calculate_avg_grade() == other.calculate_avg_grade()
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,6 +48,23 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def __str__(self):
+        return f'''Имя: {self.name}
+Фамилия: {self.surname}
+Средняя оценка за лекции: {self.calculate_avg_grade()}'''
+
+    def calculate_avg_grade(self):
+        if not self.grades:
+            return 0
+        all_grades = [grade for grades in self.grades.values() for grade in grades]
+        return sum(all_grades) / len(all_grades)
+
+    def __lt__(self, other):
+        return self.calculate_avg_grade() < other.calculate_avg_grade()
+
+    def __eq__(self, other):
+        return self.calculate_avg_grade() == other.calculate_avg_grade()
 
 
 class Reviewer(Mentor):
@@ -43,3 +79,85 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'''Имя: {self.name}
+Фамилия: {self.surname}'''
+
+
+#-------------------------------------------------------------------
+#Глобальные функции
+def calculate_avg_hw(students, course):
+    grades = []
+    for student in students:
+        if course in student.grades:
+            grades.extend(student.grades[course])
+    if not grades:
+        return 0
+    return sum(grades) / len(grades)
+
+
+def calculate_avg_lecture(lecturers, course):
+    grades = []
+    for lecturer in lecturers:
+        if course in lecturer.grades:
+            grades.extend(lecturer.grades[course])
+    if not grades:
+        return 0
+    return sum(grades) / len(grades)
+
+#-------------------------------------------------------------------
+#Проверки
+student1 = Student('Алексей', 'Иванов', 'М')
+student1.courses_in_progress = ['Python', 'Java']
+student1.finished_courses = ['Введение в программирование']
+
+student2 = Student('Мария', 'Петрова', 'Ж')
+student2.courses_in_progress = ['Python', 'JavaScript']
+student2.finished_courses = ['Введение в программирование']
+
+lecturer1 = Lecturer('Алексей', 'Сидоров')
+lecturer1.courses_attached = ['Python']
+
+lecturer2 = Lecturer('Ольга', 'Иванова')
+lecturer2.courses_attached = ['Java']
+
+reviewer1 = Reviewer('Дмитрий', 'Петров')
+reviewer1.courses_attached = ['Python']
+
+reviewer2 = Reviewer('Елена', 'Смирнова')
+reviewer2.courses_attached = ['Java']
+
+# Выставляем оценки ученикам
+
+reviewer1.rate_hw(student1, 'Python', 8)
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student2, 'Python', 10)
+
+reviewer2.rate_hw(student1, 'Java', 7)
+reviewer2.rate_hw(student2, 'JavaScript', 8)
+
+# Выставляем оценки ученикам
+
+student1.rate_lecturer(lecturer1, 'Python', 9)
+student2.rate_lecturer(lecturer1, 'Python', 10)
+
+# Выводим информацию
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
+
+# Сравниваем студентов и лекторов
+print(student1 > student2)
+print(lecturer1 < lecturer2)
+
+students = [student1, student2]
+lecturers = [lecturer1, lecturer2]
+
+print(calculate_avg_hw(students, 'Python'))
+print(calculate_avg_lecture(lecturers, 'Python'))
+
+
